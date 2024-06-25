b0cdde3603036f425b27792aae2598bb72eb987b
lukelili <lipeng5231@163.com>
2024-06-25 14:40:47 +0800

    乌七八黑

---
+++ apps/pluginAdmin/src/views/product/product.vue
@@ -0,0 +1,12 @@
+<template>
+  <el-dialog v-model="dialog.visible" title="设备模型">
+
+  </el-dialog>
+</template>
+<script setup lang="ts">
+import {reactive} from "vue";
+
+const dialog = reactive({
+  visible: true
+})
+</script>
